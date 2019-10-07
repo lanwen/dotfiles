3a7c2e599771481078dcf193ad05dd74e498a9e7
Kirill Merkushev <lanwen@yandex.ru>
2019-10-07 18:05:56 +0200

    Update random.sh

--- random.sh
+++ random.sh
@@ -1,3 +1,4 @@
-WIDTH=${1:-32}
-NUM=${2:-1}
-env LC_CTYPE=C tr -dc 'a-zA-Z0-9' < /dev/urandom | fold -w ${WIDTH} | head -n ${NUM}
+random-string()
+{
+    cat /dev/urandom | env LC_CTYPE=C tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n ${1:-1}
+}
